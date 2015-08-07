e47a7484d7a006f844742a4cdedb59e59b7e20f6
Ariel Salomon <ariel@wi.co>
2015-08-07 12:33:09 -0700

    Some bugfixes, additional functionality; Makefile to fetch SDK

--- newrelic/newrelic.go
+++ newrelic/newrelic.go
@@ -1,12 +1,14 @@
+// +build linux
+
 // Go wrapper for the New Relic Agent SDK. Requires Linux and the SDK headers
 // and libraries.
 package newrelic
 
 /*
 #cgo LDFLAGS: -L/usr/local/lib -lnewrelic-collector-client -lnewrelic-common -lnewrelic-transaction
-#include "newrelic_collector_client.h"
-#include "newrelic_common.h"
-#include "newrelic_transaction.h"
+#include "nr_agent_sdk/include/newrelic_collector_client.h"
+#include "nr_agent_sdk/include/newrelic_common.h"
+#include "nr_agent_sdk/include/newrelic_transaction.h"
 #include "stdlib.h"
 */
 import "C"
@@ -18,21 +20,21 @@ import (
 )
 
 var statusMap = map[int]string{
-	{0, "ok"},
-	{-0x10001, "other"},
-	{-0x20001, "disabled"},
-	{-0x30001, "invalid param"},
-	{-0x30002, "invalid id"},
-	{-0x40001, "transaction not started"},
-	{-0x40002, "transaction in progress"},
-	{-0x40003, "transaction not named"},
+	0: "ok",
+	-0x10001: "other",
+	-0x20001: "disabled",
+	-0x30001: "invalid param",
+	-0x30002: "invalid id",
+	-0x40001: "transaction not started",
+	-0x40002: "transaction in progress",
+	-0x40003: "transaction not named",
 }
 
 func nrError(i C.int, name string) error {
 	if int(i) < 0 {
 		status, ok := statusMap[int(i)]
 		if !ok {
-			status := "unknown"
+			status = "unknown"
 		}
 		return errors.New(fmt.Sprintf("newrelic: %s: %s", name, status))
 	}
@@ -65,11 +67,56 @@ func BeginTransaction() int64 {
 	return int64(id)
 }
 
+func SetTransactionTypeWeb(txnID int64) error {
+	rv := C.newrelic_transaction_set_type_web(C.long(txnID))
+	return nrError(rv, "set transaction type web")
+}
+
+func SetTransactionTypeOther(txnID int64) error {
+	rv := C.newrelic_transaction_set_type_other(C.long(txnID))
+	return nrError(rv, "set transaction type other")
+}
+
 func SetTransactionName(txnID int64, name string) error {
 	cname := C.CString(name)
 	defer C.free(unsafe.Pointer(cname))
 	rv := C.newrelic_transaction_set_name(C.long(txnID), cname)
 	return nrError(rv, "set transaction name")
+}
+
+func SetTransactionCategory(txnID int64, category string) error {
+	ccategory := C.CString(category)
+	defer C.free(unsafe.Pointer(ccategory))
+	rv := C.newrelic_transaction_set_category(C.long(txnID), ccategory)
+	return nrError(rv, "set transaction category")
+}
+
+func NoticeTransactionError(
+	txnID int64,
+	exception_type,
+	error_message,
+	stack_trace,
+	delimiter string) error {
+	cexception_type := C.CString(exception_type)
+	defer C.free(unsafe.Pointer(cexception_type))
+	cerror_message := C.CString(error_message)
+	defer C.free(unsafe.Pointer(cerror_message))
+	cstack_trace := C.CString(stack_trace)
+	defer C.free(unsafe.Pointer(cstack_trace))
+	cdelimiter := C.CString(delimiter)
+	defer C.free(unsafe.Pointer(cdelimiter))
+	rv := C.newrelic_transaction_notice_error(C.long(txnID),
+	  cexception_type, cerror_message, cstack_trace, cdelimiter)
+	return nrError(rv, "notice transaction error")
+}
+
+func AddTransactionAttribute(txnID int64, name, value string) error {
+	cname := C.CString(name)
+	defer C.free(unsafe.Pointer(cname))
+	cvalue := C.CString(value)
+	defer C.free(unsafe.Pointer(cvalue))
+	rv := C.newrelic_transaction_add_attribute(C.long(txnID), cname, cvalue)
+	return nrError(rv, "add transaction attribute")
 }
 
 func BeginGenericSegment(txnID int64, parentID int64, name string) int64 {
@@ -85,8 +132,7 @@ func BeginDatastoreSegment(
 	table string,
 	operation string,
 	sql string,
-	rollupName string,
-) int64 {
+	rollupName string) int64 {
 	ctable := C.CString(table)
 	defer C.free(unsafe.Pointer(ctable))
 	coperation := C.CString(operation)
